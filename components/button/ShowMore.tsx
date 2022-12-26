99f3da222651d47ce0f549c9ee1ba2fc4c00f9bb
Rono-Koushique <rono2170@gmail.com>
2022-12-27 01:58:18 +0600

    post fix

--- components/button/ShowMore.tsx
+++ components/button/ShowMore.tsx
@@ -1,15 +1,10 @@
-interface Props {
-    loadMore:Function
-}
-
-export default function ShowMore({ loadMore }: Props) {
+export default function ShowMore() {
     return (
         <div className="flex mt-12 items-center">
             <span className="flex-grow bg-gray-200 h-px" />
             <button
                 className="font-libre font-light text-gray-500 border border-gray-200 whitespace-nowrap rounded px-8 py-2
                         hover:bg-gray-50"
-                onClick={()=>loadMore()}
             >
                 Show More
             </button>
--- components/feeds/PostFeed.tsx
+++ components/feeds/PostFeed.tsx
@@ -9,29 +9,6 @@ interface Props {
 
 export default function PostFeed({ posts }: Props) {
     const [allPosts, setAllPosts] = React.useState<Post[]>([...posts]);
-    const [page, setPage] = React.useState<number>(1);
-
-    function loadMore() {
-        setPage((prev) => prev + 1);
-    }
-
-    async function addPosts() {
-        if (page > 1) {
-            const res = await (
-                await fetch(`http://localhost:3000/api/posts/${page}`)
-            ).json();
-            const newPosts = res.posts;
-            const pageCount = Math.ceil(res.pageCount);
-
-            if (page <= pageCount) {
-                setAllPosts((prev) => [...prev, ...newPosts]);
-            }
-        }
-    }
-
-    React.useEffect(() => {
-        addPosts();
-    }, [page]);
 
     return (
         <div>
@@ -40,7 +17,7 @@ export default function PostFeed({ posts }: Props) {
                     return <SinglePost post={post} key={post._id} />;
                 })}
             </div>
-            <ShowMore loadMore={loadMore} />
+            <ShowMore />
         </div>
     );
 }
